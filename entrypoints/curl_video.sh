e0e21cf10944c5ad1050d27bcb5e099faba0c749
root <582772951@qq.com>
2025-07-16 14:43:13 +0000

    fix: flux nunchaku bug

--- entrypoints/curl_video.sh
+++ entrypoints/curl_video.sh
@@ -10,8 +10,7 @@ OUTPUT_FILE="$TMP_DIR/output_$(date +"%Y%m%d_%H%M%S").bin"
 {
     echo '{'
     echo '"prompt": "Two anthropomorphic cats in comfy boxing gear and bright gloves fight intensely on a spotlighted stage.",'
-    echo '"negative_prompt": "色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，
-毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人 很多，倒着走",'
+    echo '"negative_prompt": "色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人 很多，倒着走",'
     echo '"width": 1280,'
     echo '"height": 720,'
     echo '"num_frames": 81',
